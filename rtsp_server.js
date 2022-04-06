a430854cc4f428ad7f7fe7097dea1a7446723176
RaisHelmy <44133630+RaisHelmy@users.noreply.github.com>
2022-04-06 13:32:50 +0800

    commit

--- rtsp_server.js
+++ rtsp_server.js
@@ -2,7 +2,7 @@ Stream = require('node-rtsp-stream')
 stream = new Stream({
   name: 'rtsp_server_name',
   streamUrl: 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4',
-  wsPort: 9999,
+  wsPort: 80,
   ffmpegOptions: { // options ffmpeg flags
     '-stats': '', // an option with no neccessary value uses a blank string
     '-r': 30 // options with required values specify the value after the key
